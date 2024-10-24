9388e68c1784724aecc6e7fb662e66c1f6216fd1
koo <nicewdjql@naver.com>
2024-10-24 19:34:24 +0900

    Cart Product Update

--- Model/Cart.js
+++ Model/Cart.js
@@ -12,24 +12,24 @@ const cartSchema = Schema({
         },
         size : {
             type : String,
-            required : ture
+            required : true
         },
         qty : {
             type : Number,
             default : 1,
-            required : ture
+            required : true
 
         }
     }]
 }, {timestamps:true});
 
-productSchema.methods.toJSON = function() {
-    const obj = this._doc
-    delete obj.password
-    delete obj.__v
-    delete obj.updateAt
-    delete obj.createAt
-    return obj
+cartSchema.methods.toJSON = function() {
+    const obj = this._doc;
+    delete obj.password;
+    delete obj.__v;
+    delete obj.updatedAt; 
+    delete obj.createdAt;
+    return obj;
 }
 
 const Cart = mongoose.model("Cart", cartSchema);
--- Model/Product.js
+++ Model/Product.js
@@ -41,12 +41,12 @@ const productSchema = Schema({
 }, {timestamps:true});
 
 productSchema.methods.toJSON = function() {
-    const obj = this._doc
-    delete obj.password
-    delete obj.__v
-    delete obj.updateAt
-    delete obj.createAt
-    return obj
+    const obj = this._doc;
+    delete obj.password;
+    delete obj.__v;
+    delete obj.updatedAt; 
+    delete obj.createdAt;
+    return obj;
 }
 
 const Product = mongoose.model("Product", productSchema);
